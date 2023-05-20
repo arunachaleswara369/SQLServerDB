c30d1e993788b84eab918bee9c2b5a2af417209f
Damodar Polani <aruneswari369@gmail.com>
2023-05-20 20:55:53 +0530

    Added Designation Column to Products Table

--- SqlServerDatabase/Product.sql
+++ SqlServerDatabase/Product.sql
@@ -1,7 +1,8 @@
-﻿CREATE TABLE [dbo].[Product1]
+﻿CREATE TABLE [dbo].[Product]
 (
 	[Id] INT NOT NULL PRIMARY KEY, 
     [Name] VARCHAR(50) NULL, 
     [Cost] INT NULL, 
-    [Color] NVARCHAR(50) NULL
+    [Color] NVARCHAR(50) NULL,
+    [Designation] NVARCHAR(50) NULL
 )
